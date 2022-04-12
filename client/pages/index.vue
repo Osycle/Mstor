72c20cea29a7f5e475b85020c7c24a95603e3f33
Osycle <osycle@gmail.com>
2022-04-12 18:14:31 +0500

    20220412

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -17,7 +17,7 @@
     <div class="container-fluid">
       <div class="main-wrapper">
         <div class="cells-content">
-          <div class="cells">
+          <div class="cells" v-if="cells.length">
             <div class="cell a-scale" v-for="(cell, key) in cells" :key="key" :style="'--i: '+key">
               <div class="tags-content">
                 <span v-for="(item, key) in cell.tags" :key="key" class="tags-item">
@@ -46,6 +46,11 @@
               </div>
             </div>
           </div>
+          <div v-else class="cells-empty">
+            <div>
+              <p>Ничего не найдено</p>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -53,7 +58,7 @@
     <div class="fixpanel">
       <div id="search" class="search">
         <div class="search-content">
-          <input type="text" name="" @keyup="search(search_query, $event)" v-model="search_query">
+          <input type="search" name="search" @keyup="search(search_query, $event)" v-model="search_query">
           <div class="icon-content">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path d="M19.7555474,18.6065254 L16.3181544,15.2458256 L16.3181544,15.2458256 L16.2375905,15.1233001 C16.0877892,14.9741632 15.8829641,14.8901502 15.6691675,14.8901502 C15.4553709,14.8901502 15.2505458,14.9741632 15.1007444,15.1233001 L15.1007444,15.1233001 C12.1794834,17.8033337 7.6781476,17.94901 4.58200492,15.4637171 C1.48586224,12.9784243 0.75566836,8.63336673 2.87568494,5.31016931 C4.99570152,1.9869719 9.30807195,0.716847023 12.9528494,2.34213643 C16.5976268,3.96742583 18.4438102,7.98379036 17.2670181,11.7275931 C17.182269,11.9980548 17.25154,12.2921761 17.4487374,12.4991642 C17.6459348,12.7061524 17.9410995,12.794561 18.223046,12.7310875 C18.5049924,12.667614 18.7308862,12.4619014 18.8156353,12.1914397 L18.8156353,12.1914397 C20.2223941,7.74864367 18.0977423,2.96755391 13.8161172,0.941057725 C9.53449216,-1.08543846 4.38083811,0.250823958 1.68905427,4.08541671 C-1.00272957,7.92000947 -0.424820906,13.1021457 3.0489311,16.2795011 C6.5226831,19.4568565 11.8497823,19.6758854 15.5841278,16.7948982 L18.6276529,19.7705177 C18.9419864,20.0764941 19.4501654,20.0764941 19.764499,19.7705177 C20.0785003,19.4602048 20.0785003,18.9605974 19.764499,18.6502845 L19.764499,18.6502845 L19.7555474,18.6065254 Z"/></svg>
           </div>
@@ -81,6 +86,7 @@ export default {
       test: [],
       data: null,
       cells: [],
+      cells_init: [],
       search_query: "",
       all_tags: [],
     }
@@ -237,21 +243,23 @@ export default {
     async search(query, event){
       var vm = this
       console.log(query, event)
-      // if(query.length < 3)
-      //   return
-      // return;
-      const response = await this.$axios.$post("/handler.py", {
-        action: "search",
-        query,
-      })
-      
-      if(response.status == true){
-        if(response.count > 0){
-          this.cells = response.cells
-        }else{
-          this.cells = this.cells_init
-        }
+      if(query.length < 3){
+        this.cells = this.cells_init
+        return;
       }
+
+      if(window.searchTimeout)
+        clearTimeout(window.searchTimeout)
+        
+      window.searchTimeout = setTimeout(async ()=>{
+        const response = await vm.$axios.$post("/handler.py", {
+          action: "search",
+          query,
+        })
+        if(response.status == true){
+          vm.cells = response.cells
+        }
+      }, 300);
     }
   },
   
