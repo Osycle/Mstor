cf96c3b47c35b08f0e1152713a4cf8e8afda1bc6
Азат <osycle@gmail.com>
2023-10-20 18:04:19 +0500

    fix bug

--- server/joda/joda/urls.py
+++ server/joda/joda/urls.py
@@ -11,6 +11,7 @@ router.register(r"tags", TagsViewSet, basename='tags')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('', include('oauth.urls')),
+    # path('', include('oauth.urls')),
+    path('api/v1/l/', include('rest_framework.urls')),
 
 ]
